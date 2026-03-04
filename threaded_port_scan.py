43da93728038a5299c5e64701defc9ff33fe2271
tbaaaa <ayromulus8@gmail.com>
2026-03-03 20:10:38 -0400

    Implementation of threading by introducing the scan_thread function using queues

--- threaded_port_scan.py
+++ threaded_port_scan.py
@@ -14,3 +14,26 @@ RESET = Fore.RESET # reset to default color
 NUM_OF_THREADS = 200
 q = Queue()
 print_lock = Lock()
+
+def port_scan(port):
+    # Scan a port on the global variable `host`
+    try:
+        s = socket.socket()
+        s.connect((host, port))
+    except:
+        with print_lock:
+            print(f"{GRAY}{host:15}:{port:5} is closed {RESET}", end="\r")
+    else:
+        with print_lock:
+            print(f"{GREEN}{host:15}:{port:5} is open {RESET}")
+    finally:
+        s.close()
+
+def scan_thread():
+    global q
+    while True:
+        # get the port number from the queue
+        worker = q.get()
+        #scan that port number
+        port_scan(worker)
+        q.task_done()
