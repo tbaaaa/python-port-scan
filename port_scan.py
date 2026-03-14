56506d52f7d77afffb5714ea9bbbdd8bca6f31a0
tbaaaa <ayromulus8@gmail.com>
2026-03-13 21:24:38 -0400

    Refactored broken code; added parameters for socket object creation; corrected settimeout execution; close socket after each port is scanned (saves resources); added logic to handle user input for host name

--- port_scan.py
+++ port_scan.py
@@ -1,34 +1,38 @@
-import socket #module for connections
-import ipaddress #module for validating IP addresses
-from colorama import init, Fore #module for colored text
+import socket # module for connections
+import ipaddress # module for validating IP addresses
+from colorama import init, Fore # module for colored text
 
 init() #initialize colorama
-GREEN = Fore.GREEN #Print in green if port is open
-GRAY = Fore.LIGHTBLACK_EX #Print in gray if port is closed
-RESET = Fore.RESET #Reset to default color
+GREEN = Fore.GREEN # Print in green if port is open
+GRAY = Fore.LIGHTBLACK_EX # Print in gray if port is closed
+RESET = Fore.RESET # Reset to default color
 
 def is_port_open(host, port):
-    """Check if a specific port is open on a given host"""
+    #Check if a specific port is open on a given host
     #creates a new socket object
-    s = socket.socket()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.AF_INET & socket.SOCK_STREAM = Use IPv4 TCP socket
+    # if target slow or unresponsive, give up after 1 sec
+    s.settimeout(1)
     try:
-        #tries to connect to the specified host using that port
+        # tries to connect to the specified host using that port
         s.connect((host, port))
-
-        #set a timeout for the connection attempt
-        s.settimeout(0.2)
     except:
         # cannot connect to the port; port is closed
         return False
     else:
         # connection successful; port is open
         return True
+    finally:
+        # closes socket after connection attempt
+        s.close()
 
 # get host from user input
 host = input("Enter the host IP address or domain name to scan: ")
+ip = socket.gethostbyname(host) # also accept IPs (user can also enter a valid IP; method will return it)
+
 try:
     # Validate the host input
-    ipaddress.ip_address(host) # Check if it's a valid IP address
+    ipaddress.ip_address(ip) # Check if it's a valid IP address
 except ValueError:
     print(f"{GRAY}[-] Invalid IP address{RESET}")
     exit()
